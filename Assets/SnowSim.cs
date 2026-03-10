23a41a9c620151ad585e5cc15a5e07c559b41ce5
Ignasi Myrsky Vendrell <11618271+stereoknife@users.noreply.github.com>
2026-03-10 18:05:27 +0100

    unsure whats here but i need to sync

---
+++ Assets/SnowSim.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class SnowSim : MonoBehaviour
+{
+    private Terrain terrain;
+    private NativeArray<ushort> snowHeightmap;
+
+    private void Awake()
+    {
+        terrain = GetComponent<Terrain>();
+        snowHeightmap = new NativeArray<ushort>(terrain.Xsize * terrain.Zsize, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
+        snowHeightmap[(terrain.Xsize/2) * (terrain.Zsize/2)] = 1 << 12;
+    }
+
+    private void Update()
+    {
+        var job = new SnowSimJob
+        {
+            inputHeightmap =  new NativeArray<ushort>(snowHeightmap, Allocator.TempJob),
+            outputHeightmap = snowHeightmap,
+            width = terrain.Xsize,
+        };
+
+        var handle = job.ScheduleParallel(snowHeightmap.Length, 32, new JobHandle());
+        handle.Complete();
+    }
+    
+    private struct SnowSimJob : IJobFor
+    {
+        [ReadOnly] public NativeArray<ushort> inputHeightmap;
+        public NativeArray<ushort> outputHeightmap;
+        public int width;
+    
+        public void Execute(int index)
+        {
+            int4 nidx = new(index + 1, index - 1, index + width, index - width);
+            int4 nval = new();
+            ushort thisval = inputHeightmap[index];
+
+            bool4 nbounds = nidx > 0 & nidx < inputHeightmap.Length & nidx % width != 0 & nidx % width != width - 1;
+        
+            if (nbounds.x) nval.x = inputHeightmap[nidx.x];
+            if (nbounds.y) nval.y = inputHeightmap[nidx.y];
+            if (nbounds.z) nval.z = inputHeightmap[nidx.z];
+            if (nbounds.w) nval.w = inputHeightmap[nidx.w];
+        
+            bool4 exchange = nval < thisval;
+        
+            nval = math.select(nval, nval + 1, exchange);
+
+            if (exchange.x) thisval--;
+            if (exchange.y) thisval--;
+            if (exchange.z) thisval--;
+            if (exchange.w) thisval--;
+
+            outputHeightmap[index] = thisval;
+            outputHeightmap[nidx.x] = (ushort)nval.x;
+            outputHeightmap[nidx.y] = (ushort)nval.y;
+            outputHeightmap[nidx.z] = (ushort)nval.z;
+            outputHeightmap[nidx.w] = (ushort)nval.w;
+        }
+    }
+}
---
+++ Assets/TerrainRenderer.cs
@@ -0,0 +1,90 @@
+﻿using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+[RequireComponent(typeof(Terrain))]
+public class TerrainRenderer : MonoBehaviour
+{
+    [SerializeField] private Material material, snow;
+    [SerializeField] private Mesh cube;
+    private Terrain terrain;
+    private NativeArray<Matrix4x4> instanceData;
+
+    private void Awake()
+    {
+        terrain = GetComponent<Terrain>();
+        instanceData = new NativeArray<Matrix4x4>(terrain.Xsize * terrain.Zsize, Allocator.Persistent);
+    }
+
+    private void Update()
+    {
+        var job = new InstanceTransformJob
+        {
+            heightmap = terrain.GetHeightmapData(),
+            height = terrain.Height,
+            instanceData = instanceData,
+            resolution = terrain.Xsize,
+        };
+
+        var handle = job.ScheduleParallel(instanceData.Length, 32, new JobHandle());
+        handle.Complete();
+        
+        var rp = new RenderParams(material);
+        Graphics.RenderMeshInstanced(rp, cube, 0, instanceData);
+        
+        var snowjob = new SnowInstanceTransformJob
+        {
+            heightmap = terrain.GetHeightmapData(),
+            height = terrain.Height,
+            instanceData = instanceData,
+            resolution = terrain.Xsize,
+        };
+
+        handle = job.ScheduleParallel(instanceData.Length, 32, handle);
+        handle.Complete();
+        
+        rp = new RenderParams(material);
+        Graphics.RenderMeshInstanced(rp, cube, 0, instanceData);
+    }
+    
+    private struct InstanceTransformJob : IJobFor
+    {
+        public NativeArray<Matrix4x4> instanceData;
+        [ReadOnly] public NativeArray<ushort> heightmap;
+        public int resolution;
+        public float height;
+    
+        public void Execute(int index)
+        {
+            var i = index / resolution;
+            var j = index % resolution;
+            var h = ((float)heightmap[index]/(float)ushort.MaxValue) * height;
+            instanceData[index] = Matrix4x4.TRS(
+                new Vector3(i, h * 0.5f, j),
+                Quaternion.identity,
+                new Vector3(1, h, 1)
+            );
+        }
+    }
+    
+    private struct SnowInstanceTransformJob : IJobFor
+    {
+        public NativeArray<Matrix4x4> instanceData;
+        [ReadOnly] public NativeArray<ushort> heightmap;
+        [ReadOnly] public NativeArray<ushort> baseHeightmap;
+        public int resolution;
+        public float height;
+    
+        public void Execute(int index)
+        {
+            var i = index / resolution;
+            var j = index % resolution;
+            var h = ((float)heightmap[index]/(float)ushort.MaxValue) * height;
+            instanceData[index] = Matrix4x4.TRS(
+                new Vector3(i, h * 0.5f + baseHeightmap[index], j),
+                Quaternion.identity,
+                new Vector3(1, h, 1)
+            );
+        }
+    }
+}
